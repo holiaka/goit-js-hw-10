f7529e0378ff1804092bbc347fbd83ce5f009dd3
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-22 00:09:13 +0200

    Final  ver.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+// Libries import
 import './css/styles.css';
 import debounce from "lodash.debounce";
 import { fetchCountries } from "./js/fetchCountries";
 
+// Key var.
 const DEBOUNCE_DELAY = 300;
 
-const inputRef = document.querySelector('#search-box');
-    
+// Input link
+const inputRef = document.querySelector('#search-box');    
 inputRef.addEventListener("input", debounce(onTypingText, 300));
 
+// Event listener callback func.
 function onTypingText(evt) {
     let countryName = evt.target.value.trim();
     fetchCountries(countryName);    
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,75 +1,94 @@
+// Load Notiflix library
 import Notiflix from 'notiflix';
 
+// links to HTML elements
 const countryListRef = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+//Hide data about country (-ies)
+hiddenCountryDiscr();
+
+// Fetch func.
 export function fetchCountries(name) {
   fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages,area,latlng,capitalInfo`
   )
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status)
+        throw new Error(response.status);
       }
       console.dir(response);
-      return response.json()
+      return response.json();
     })
     .then(data => {
-      preprocessingData(data)
+      preprocessingData(data);
     })
     .catch(error => {
-      onFetchError(error)
+      onFetchError(error);
     });
 }
 
+/*      Support functions list        */
 function preprocessingData(data) {
-  countryListRef.innerHTML = '';
-  countryInfo.innerHTML = '';
+  hiddenCountryDiscr();
 
   if (data.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
   } else if (data.length > 1 && data.length <= 10) {
-    data.map(item => {
-      let {
-        name: { official: country },
-        flags: { svg: flag },
-      } = item;
-      let htmlLiElements = `<li class="country-item"><img src="${flag}" alt="Country flag" width="50" height ="25"><p>${country}</p></li>`;
-      countryListRef.innerHTML += htmlLiElements;
-    });
+    createCountriesList(data);
   } else {
-    countryInfo.removeAttribute('js-hidden');
+    createCountyDiscription(data);
+  }
+}
+
+function onFetchError(error) {
+  hiddenCountryDiscr();
+
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function hiddenCountryDiscr() {
+  countryListRef.innerHTML = '';
+  countryInfo.innerHTML = '';
+  countryInfo.setAttribute('js-hidden', '');
+}
+
+function createCountriesList(data) {
+  data.map(item => {
     let {
       name: { official: country },
-      capital,
-      population,
       flags: { svg: flag },
-      languages: lang,
-      area,
-      latlng: countryCoordinats,
-      capitalInfo: { latlng: capitalCoordinats },
-    } = iter;
+    } = item;
+    let htmlLiElements = `<li class="country-item"><img src="${flag}" alt="Country flag" width="50" height ="25"><p>${country}</p></li>`;
+    countryListRef.innerHTML += htmlLiElements;
+  });
+}
 
-    let htmlInfo = `<img src="${flag}" alt="Country flag" width="100"><h1>${country}</h1>
+function createCountyDiscription(data) {
+  countryInfo.removeAttribute('js-hidden');
+  let {
+    name: { official: country },
+    capital,
+    population,
+    flags: { svg: flag },
+    languages: lang,
+    area,
+    latlng: countryCoordinats,
+    capitalInfo: { latlng: capitalCoordinats },
+  } = data[0];
+
+  let htmlInfo = `<img src="${flag}" alt="Country flag" width="100"><h1>${country}</h1>
       <p class="disc-text">Capital: ${capital.join(', ')}</p>
       <p class="disc-text">Population: ${(population / 1000000).toFixed(
         2
       )} M people</p>
       <p class="disc-text">Language(s): ${Object.values(lang).join(', ')}</p>
       <div id="map"></div>`;
-    countryInfo.innerHTML = htmlInfo;
+  countryInfo.innerHTML = htmlInfo;
 
-    createMap(country, capital, area, countryCoordinats, capitalCoordinats);
-  }
-}
-
-function onFetchError(error) {
-  countryListRef.innerHTML = '';
-  countryInfo.innerHTML = '';
-
-  Notiflix.Notify.failure('Oops, there is no country with that name');
+  createMap(country, capital, area, countryCoordinats, capitalCoordinats);
 }
 
 function createMap(
@@ -81,7 +100,7 @@ function createMap(
 ) {
   var map = L.map('map').setView(
     countryCoordinats,
-    Math.round(-0.74 * Math.log(area) + 13)
+    Math.round(-0.74 * Math.log(area) + 14)
   );
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
